a18334187ed44718dc3ca2d7aadbad22c8cd8906
bisal-xetri <bishaldkl56@gmail.com>
2024-09-01 11:14:22 +0545

    consuming promises

--- script.js
+++ script.js
@@ -34,24 +34,31 @@ getCountryData('nepal')
 getCountryData('nepal')
 
 */
-const renderCountry=function(data, className=''){
-
-   
+const renderCountry = function (data, className = '') {
   const html = `
   <article class="country ${className}">
-    <img class="country__img" src="${data.flags.png}" alt="Flag of ${data.name.common}" />
+    <img class="country__img" src="${data.flags.png}" alt="Flag of ${
+    data.name.common
+  }" />
     <div class="country__data">
       <h3 class="country__name">${data.name.common}</h3>
       <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} million people</p>
-      <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
-      <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} million people</p>
+      <p class="country__row"><span>🗣️</span>${
+        Object.values(data.languages)[0]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        Object.values(data.currencies)[0].name
+      }</p>
     </div>
   </article>`;
-  
+
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
-}
+};
+/*
 const getCountryAndNeighbor=function(country){
 const request = new XMLHttpRequest();
 request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
@@ -94,3 +101,31 @@ setTimeout(() => {
     }, 1000);
   }, 1000);
 }, 1000);
+*/
+
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+// request.send();
+
+const request = fetch('https://restcountries.com/v3.1/name/nepal');
+console.log(request);
+
+//promises are placeholder for future value/ response coming from AJAX call
+
+// const getCountryData=function(country){
+//   fetch(`https://restcountries.com/v3.1/name/${country}`).then(function(response){
+
+//     console.log(response);
+//     return response.json();
+//   }).then(function(data){
+//     console.log(data)
+//     renderCountry(data[0])
+//   })
+
+// }
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+getCountryData('nepal');
